a6845bc7af46efe36d50586733d79d3a23f9c859
Miguel Garcia Marques <miguelgarciamarques102@gmail.com>
2023-12-05 20:05:19 +0000

    added result as number

--- logic.js
+++ logic.js
@@ -1,11 +1,6 @@
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 window.onload =function generateString() {
-    let length = 600;
-    let result = ' ';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
+    let result = Math.floor(Math.random() * charactersLength);
     document.getElementById("frase").innerHTML = result;
 }
