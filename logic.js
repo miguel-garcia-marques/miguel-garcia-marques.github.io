a4818b4e4ba2ef63678cc8c3cf408a89c19c0d9c
Miguel Garcia Marques <miguelgarciamarques102@gmail.com>
2023-12-05 20:11:50 +0000

    correct error

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 window.onload = function generateString() {
-    let result = Math.floor(Math.random() * charactersLength);
+    let result = Math.floor(Math.random() * 10);
     console.log(result);
     document.getElementById("frase").innerHTML = result.toString;
 }
